0a445fd1eba7b47300394b00cb9e7c2306d4e5bb
mamun747 <technoultra42@gmail.com>
2025-02-18 23:16:36 +0600

    table created successfully

--- src/components/common/button.vue
+++ src/components/common/button.vue
@@ -3,7 +3,7 @@ import Close from "../icons/icon.vue";
 </script>
 <template>
   <button
-    class="flex gap-1 bg-[#295A41] font-bold items-center px-1.5 py-0.5 rounded-md cursor-pointer"
+    class="flex gap-1 bg-[#295A41] text-slate-300 font-bold items-center px-1.5 rounded-md cursor-pointer"
   >
     <slot></slot>
     <Close />
