e578bb3c618c640fbb4ee88840dc0054ff6220e6
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-02-19 16:29:59 +0600

    Enhance footer responsiveness with mobile-friendly design

--- src/components/data-table/item.vue
+++ src/components/data-table/item.vue
@@ -15,20 +15,19 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="grid grid-cols-5 w-full bg-[#0d0d0f] py-1.5 text-white items-center ps-3 pe-2 border-b border-[#1f1f1f]"
-  >
-    <div>
-      <div class="flex gap-11 items-center">
+  <div>
+    <div
+      class="grid grid-cols-5 w-full bg-[#0d0d0f] py-1.5 text-white items-center ps-3 pe-2 border-b border-[#1f1f1f] min-w-2xl"
+    >
+      <div class="flex gap-11 items-center min-w-fit w-fit">
         <div
           class="w-4 h-4 rounded-sm bg-gray-800/80 border border-gray-600 cursor-pointer"
-        >
+        ></div>
+        <p class="text-[#319b64] text-xs text-nowrap md:text-sm font-semibold">
+          {{ createdAgo }}
+        </p>
       </div>
-        <p class="text-[#319b64] text-xs text-nowrap md:text-sm font-semibold">{{ createdAgo }}</p>
-      </div>
-    </div>
-    <div>
-      <div class="flex gap-0.5 items-center text-slate-300">
+      <div class="flex gap-0.5 items-center text-slate-300 min-w-fit w-fit">
         <img
           src="/apple.png"
           class="h-6 w-6 border border-gray-800 rounded-md p-1"
@@ -38,23 +37,19 @@ defineProps({
         <p class="text-[#5C5C5E] font-semibold text-sm">{{ address }}</p>
         <copy />
       </div>
-    </div>
-    <div>
-      <div class="bg-[#874ba8]/30 inline-block rounded-md">
+      <div class="bg-[#874ba8]/30 inline-block rounded-md min-w-fit w-fit">
         <p class="uppercase px-1 py-0.5 font-semibold text-[#874ba8] text-xs">
           solana
         </p>
       </div>
-    </div>
-    <div>
       <div
-        class="border border-gray-800 rounded-md p-1 flex gap-1 max-w-[230px]"
+        class="border border-gray-800 rounded-md p-1 flex gap-1 max-w-[230px] min-w-fit w-fit"
       >
         <CommonButton v-for="tag of tags">{{ tag.name }}</CommonButton>
       </div>
-    </div>
-    <div class="flex justify-end">
-      <button2 />
+      <div class="flex justify-end min-w-fit w-full">
+        <button2 />
+      </div>
     </div>
   </div>
 </template>
